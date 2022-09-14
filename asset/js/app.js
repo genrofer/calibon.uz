4cdd567c52213539ca1a480bcf145259cac2ed81
amuzaffarbe <amuzaffarbe@gmail.com>
2022-09-14 13:29:20 +0500

    new changes

--- asset/js/app.js
+++ asset/js/app.js
@@ -4,6 +4,7 @@ let boglanish = document.querySelector(".boglanish")
 let home = document.querySelector(".home")
 
 let lang = document.querySelector(".select-lang");
+let lang2 = document.querySelector(".select-lang2");
 let select = document.querySelector(".select");
 let arrow = document.querySelector(".arrow"); 
 
@@ -36,3 +37,12 @@ function selectFunction() {
      } 
 }
 
+function selectFunction2() {
+     if (lang2.value === "uz") {
+          window.location.href = "index.html";
+     } else if (lang2.value === "ru") {
+          window.location.href = "index-ru.html";
+     } else if (lang2.value === "en") {
+          window.location.href = "index-en.html";
+     } 
+}
